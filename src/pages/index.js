985d71409f50f5653d04a8ac70401af7d6c0ccc3
Jannis <jannis@pixel-pink.de>
2018-09-02 11:36:30 +0200

    removed news component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import News from '../components/news'
 
 import Layout from '../components/layout'
 
@@ -9,7 +8,6 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <News></News>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
