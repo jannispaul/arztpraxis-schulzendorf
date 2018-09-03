df287c6f27223d487f6d7895e66b7d1ee7f7f25c
Jannis <jannis@pixel-pink.de>
2018-09-03 16:28:33 +0200

    changed content structure

--- content/times.md
+++ content/times.md
@@ -1,6 +1,8 @@
+---
 path: times
 monday: '12424'
 tuesday: '325235'
 wednesday: t423
 thursday: '124214235235'
 friday: '235'
+---
--- src/components/news.js
+++ src/components/news.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from "gatsby"
 
-
-// const News = () => (
-//   <div>
-//     <h1>NEWS:</h1>
-//     <p>Welcome to page 2</p>
-//   </div>
-// )
-
-// export default News
-
-
 export default () => (
   <StaticQuery
     query={graphql`
