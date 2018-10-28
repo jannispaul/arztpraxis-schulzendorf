549f39acabc63b31e0b03e2c881475d528556cb5
Jannis <jannis@pixel-pink.de>
2018-10-28 13:03:29 +0100

    font swap, dark green for accessibility, reorganized css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
+
+import  '../theme/font-face.css'
+import  '../theme/variables.css'
+import '../theme/styles.css'
+
 import News from '../components/news'
 import Times from '../components/times'
 import Notdienst from '../components/notdienst'
@@ -6,9 +11,7 @@ import Kontakt from '../components/kontakt'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
 
-import  '../theme/font-face.css'
-import '../theme/styles.css'
-import  '../theme/variables.css'
+
 
 
 const IndexPage = () => (
