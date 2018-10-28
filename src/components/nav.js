a1c9db093ecee18ee6eed63e7edd76d172c5e691
Jannis <jannis@pixel-pink.de>
2018-10-28 16:06:14 +0100

    Fixed font-sizing, @font-face font import

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,12 +6,12 @@ import Icon from './icon'
 import {ICONS} from '../constants/icons'
 
 const Nav = () => (
-  <nav>
+  <nav className={styles.nav}>
       <Link to="/" className={`${styles.desktop} ${styles.logo}`}>
         <img src={logo} alt="Arztpraxis Schulzendorf Logo"/>
       </Link>
-      <a href="tel:03376248550" className={styles.desktop}>
-        <Icon icon={ICONS.PHONE} color="var(--color-primary)"/>033762 48550
+      <a href="tel:03376248550" className={`${styles.desktop} ${styles.iconLink}`}>
+        <Icon icon={ICONS.PHONE} color="var(--color-primary)" className={styles.icon}/>033762 48550
       </a>
       <Link to="/leistungen" className={styles.desktop} activeClassName={styles.active}>Leistungen</Link>
       <Link to="/termine" className={`${styles.desktop} ${styles.button}`} activeClassName={styles.active}>Termin buchen</Link>
