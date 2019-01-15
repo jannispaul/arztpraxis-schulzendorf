81c9d311d4430c61dcc73504a90c4aeb2effecb2
Jannis <jannis@pixel-pink.de>
2019-01-15 16:15:53 +0100

    Added Impressum/ changed ms nav css rules

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -21,9 +21,38 @@ const Impressum = () => (
         <li>Telefon: 033762 48550</li>
         <li>Fax: 033752 48552</li>
       </ul>
-      <p>
-      
+      <h4>Berufsbezeichnung und berufsrechtliche Regelungen</h4>
+      <p>Gesetzliche Berufsbezeichnung: 
+        Arzt, Approbationen verliehen in der Bundesrepublik Deutschland
+        Fachärztin für Allgemeinmedizin
       </p>
+      <h4>Zuständige Ärztekammer:</h4>
+      <p>Landesärztekammer Brandenburg<br />
+          Geschäftsstelle Cottbus <br />
+          PF 10 14 45<br />
+          03014 Cottbus<br />
+          Web: <a href="www.laekb.de">www.laekb.de</a></p>
+           
+          <h4>Berufsrechtliche Regelung</h4>
+        <p>Berufsordnung für die Ärzte im Land Brandenburg<br />
+          Gebührenordnung für Ärzte (GOÄ)<br />
+          Heilberufsgesetz des Landes Brandenburg<br />
+          Einheitlicher Bewertungsmaßstab (EBM)<br />
+           <br />
+          Die Regelungen finden Sie unter:<br />
+          <a href="https://www.laekb.de/www/website/ServiceNavigation/download/">https://www.laekb.de/www/website/ServiceNavigation/download/</a></p>
+           
+        <h4>Aufsichtsbehörde:</h4>
+        <p>Kassenärztliche Vereinigung Brandenburg<br />
+          Pappelallee 5<br />
+          14469 Potsdam<br />
+          Web: <a href="www.kvbb.de">www.kvbb.de</a><br />
+        </p>
+        <h4>Angaben zur Arzthaftpflichtversicherung</h4>
+        <p>Name und Sitz des Versicherers:<br />
+           Deutsch Ärzteversicherung Allgemeine Zweigniederlassung der AXA Vers. AG<br />
+           Postanschrift: 51171 Köln<br />
+        </p>
       <Link to="/">Zurück zur Startseite</Link>
     </Legalcontainer>
   </Layout>
