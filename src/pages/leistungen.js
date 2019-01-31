dc1a0c26be0fed27ecd627a4e33b384de3672e0a
Jannis <jannis@pixel-pink.de>
2019-01-31 17:15:25 +0100

    added news to leistungen

--- src/pages/leistungen.js
+++ src/pages/leistungen.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import News from '../components/news'
 import ServiceHero from '../components/servicehero'
 import Service from '../components/service'
 import Contact from '../components/contact'
@@ -10,6 +11,7 @@ import Contact from '../components/contact'
 
 const Leistungen = () => (
   <Layout>
+    <News></News>
     <ServiceHero></ServiceHero>
     <Service></Service>
     <Contact></Contact>
