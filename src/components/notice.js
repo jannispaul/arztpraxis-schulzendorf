f8f610f534684a7d88e43893b74688ae6c100dd7
Jannis <jannis@pixel-pink.de>
2020-03-26 18:49:15 +0100

    No phone update

--- src/components/notice.js
+++ src/components/notice.js
@@ -19,8 +19,14 @@ const Notice = () => (
             Hause zu bleiben und nicht in die Praxis zu kommen.
             <br />
             <br />
-            Nehmen Sie zuerst telefonisch Kontakt auf: <br />
-            Arztpraxis: 033762-48550 / Wochenende: 116 117 <br />
+            Nehmen Sie bitte per E-Mail Kontakt mit uns auf: <br />
+            Arztpraxis:{' '}
+            <a href="mailto:info@arztpraxis-schulzendorf.de">
+              info@arztpraxis-schulzendorf.de
+            </a>
+            <br />
+            Oder per Telefon:
+            <br />
             Gesundheitsamt (Mo.-So. 8-18 Uhr): 03375-262146 <br />
             Bürgertelefon (Mo.-Fr. 9-15 Uhr): 0331-8683777
           </span>
